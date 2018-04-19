c86e2e6ca1cf53cd5fb5da91ab4358bad59dbbfb
俞世权 <sq.yu@morpx.com>
2018-04-19 18:37:03 +0800

    update

--- code/py_tool/pic_data_convert.py
+++ code/py_tool/pic_data_convert.py
@@ -7,11 +7,9 @@ def save_pic2npy(image_dir):
 	if not os.path.exists(image_dir) :
 		print "error, can not find directer: "+image_dir
 		exit(0)
-		# print "error path: "+sys.argv[1]
-		# exit(0)
 	image = np.zeros((1,),np.uint8)
 	for file in os.listdir(image_dir):
-		if file.endswith(".jpg"):
+		if file.endswith(".jpg") or file.endswith(".png"):
 			file_path = os.path.join(image_dir, file)
 			print file_path
 			img = cv2.imread(file_path)
@@ -20,7 +18,8 @@ def save_pic2npy(image_dir):
 				image = img
 			else:
 				image = np.vstack((image,img))
-	np.save("pic", image)
+	dir_name_list = image_dir.split('/')
+	np.save(dir_name_list[-2], image)
 
 def load_npy2pic(image_dir):
 	if os.path.exists(image_dir) :
@@ -31,11 +30,14 @@ def load_npy2pic(image_dir):
 		print "error, can not find file: "+image_dir
 		exit(0)
 	image = np.load(image_dir)
+	print image.shape
 	for i in range(image.shape[0]):
 		window_name = "image["+str(i)+"]"
 		cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)#cv2.WINDOW_AUTOSIZE)
 		cv2.imshow(window_name,image[i])
-		cv2.waitKey()
+		key = cv2.waitKey()
+		if key == 27 or key == 131099:	#27 = lower'ESC', 131099 = upper'ESC'
+			exit(0)
 		cv2.destroyAllWindows()
 	pass
 
@@ -48,13 +50,14 @@ def main():
 		save_pic2npy(args.pic)
 	if args.data != None:
 		load_npy2pic(args.data)
-	print sys.argv
-	print "args.pic = "+str(args.pic)
-	print "args.data = "+str(args.data)
+	# print sys.argv
+	# print "args.pic = "+str(args.pic)
+	# print "args.data = "+str(args.data)
 	pass
 
 if __name__ == "__main__":
 	main()
+	# image = image.ravel()`
 	# for root, dirs, files in os.walk("./"):  
 	# 	print(root)
 	# 	print(dirs)
