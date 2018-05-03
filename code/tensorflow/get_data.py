430b63def3402acc5b5dd3beee33566918a30b4b
俞世权 <sq.yu@morpx.com>
2018-05-03 18:03:35 +0800

    update

--- code/tensorflow/get_data.py
+++ code/tensorflow/get_data.py
@@ -4,6 +4,19 @@ import sys, os
 import numpy as np
 import cv2
 
+#把稠密数据label[1,5...]变为[[0,1,0,0...],[...]...]
+def dense_to_one_hot(labels_dense, num_classes):
+	#数据数量
+	num_labels = labels_dense.shape[0]
+	#生成[0,1,2...]*10,[0,10,20...]
+	index_offset = np.arange(num_labels) * num_classes
+	#初始化np的二维数组
+	labels_one_hot = np.zeros((num_labels, num_classes))
+	#相对应位置赋值变为[[0,1,0,0...],[...]...]
+	labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+
+	return labels_one_hot
+
 def isPathExist(filePath):
 	if os.path.exists(filePath) :
 		if not filePath.endswith(".npy"):
@@ -46,9 +59,9 @@ class GetMaleFemaleData(object):
 
 
 		self.train_data = np.vstack((male_train_data,female_train_data))
-		self.train_lable = np.hstack((male_train_lable,female_train_lable))
+		self.train_lable = dense_to_one_hot(np.hstack((male_train_lable,female_train_lable)),2)
 		self.test_data = np.vstack((male_test_data,female_test_data))
-		self.test_lable = np.hstack((male_test_lable,female_test_lable))
+		self.test_lable = dense_to_one_hot(np.hstack((male_test_lable,female_test_lable)),2)
 		# print 'self.train_data.shape = '+str(self.train_data.shape)
 		# print 'self.train_lable.shape = '+str(self.train_lable.shape)
 		# print 'self.test_data.shape = '+str(self.test_data.shape)
@@ -71,13 +84,13 @@ class GetMaleFemaleData(object):
 		pass
 
 def main():
-	male_dir = sys.argv[1]#'/home/ysq/YSQWork/DeepLearning/data/FemaleMaleFace_30x30/1_Male.npy'
-	female_dir = sys.argv[2]#'/home/ysq/YSQWork/DeepLearning/data/FemaleMaleFace_30x30/0_Female.npy'
+	male_dir = '/home/ysq/YSQWork/DeepLearning/data/FemaleMaleFace_30x30/1_Male.npy'	#sys.argv[1]#'/home/ysq/YSQWork/DeepLearning/data/FemaleMaleFace_30x30/1_Male.npy'
+	female_dir = '/home/ysq/YSQWork/DeepLearning/data/FemaleMaleFace_30x30/0_Female.npy'	#sys.argv[2]#'/home/ysq/YSQWork/DeepLearning/data/FemaleMaleFace_30x30/0_Female.npy'
 	data_get = GetMaleFemaleData(male_dir, female_dir)
 	# train_data, train_lable = data_get.next_train_batch(50)
 	train_data, train_lable = data_get.get_test_data()
 	cv2.namedWindow('image',cv2.WINDOW_NORMAL)
-	for i in range(train_data.shape[0]):
+	for i in xrange(train_data.shape[0]):
 		cv2.imshow('image', train_data[i])
 		print train_lable[i]
 		cv2.waitKey(0)
--- code/tensorflow/male_female_train.py
+++ code/tensorflow/male_female_train.py
@@ -24,41 +24,41 @@ def main():
 	sess = tf.InteractiveSession()
 	
 	#声明输入图片数据，类别
-	x = tf.placeholder('float',[None,30,30,3])
-	y_ = tf.placeholder('int32',[None])
+	x = tf.placeholder('float',[None,30,30,3])	
+	y_ = tf.placeholder('float',[None,2])
 	#第一层卷积层
-	W_conv1 = weight_variable([5, 5, 3, 64])
-	b_conv1 = bias_variable([64])
+	W_conv1 = weight_variable([3, 3, 3, 36])	#[5, 5, 3, 64]
+	b_conv1 = bias_variable([36])				#[64]
 	#进行卷积操作，并添加relu激活函数
 	conv1 = tf.nn.relu(conv2d(x,W_conv1) + b_conv1)
 	# pool1
-	pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')	#TODO changed get shape(15, 15)
+	pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')	#TODO changed get shape(15, 15) ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1]
 	# norm1
 	norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,name='norm1')
 	#第二层卷积层
-	W_conv2 = weight_variable([5,5,64,64])
-	b_conv2 = bias_variable([64])
+	W_conv2 = weight_variable([3,3,36,36])		#[5,5,64,64]
+	b_conv2 = bias_variable([36])				#[64]
 	conv2 = tf.nn.relu(conv2d(norm1,W_conv2) + b_conv2)
 	# norm2
 	norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,name='norm2')
 	# pool2
-	pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],strides=[1, 3, 3, 1], padding='SAME', name='pool2')	#TODO changed get shape(5, 5)
+	pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],strides=[1, 3, 3, 1], padding='SAME', name='pool2')	#TODO changed get shape(5, 5) ksize=[1, 3, 3, 1],strides=[1, 2, 2, 1]
 
 	#全连接层
 	#权值参数
-	W_fc1 = weight_variable([5*5*64,384])#([8*8*64,384])
+	W_fc1 = weight_variable([5*5*36,216])	#([8*8*64,384])
 	#偏置值
-	b_fc1 = bias_variable([384])
+	b_fc1 = bias_variable([216])
 	#将卷积的产出展开
-	pool2_flat = tf.reshape(pool2,[-1,5*5*64])
+	pool2_flat = tf.reshape(pool2,[-1,5*5*36])
 	#神经网络计算，并添加relu激活函数
 	fc1 = tf.nn.relu(tf.matmul(pool2_flat,W_fc1) + b_fc1)
 	
 	#全连接第二层
 	#权值参数
-	W_fc2 = weight_variable([384,192])
+	W_fc2 = weight_variable([216,108])
 	#偏置值
-	b_fc2 = bias_variable([192])
+	b_fc2 = bias_variable([108])
 	#神经网络计算，并添加relu激活函数
 	fc2 = tf.nn.relu(tf.matmul(fc1,W_fc2) + b_fc2)
 
@@ -68,26 +68,28 @@ def main():
 	fc1_drop = tf.nn.dropout(fc2,keep_prob)
 	
 	#输出层，使用softmax进行多分类
-	W_fc2 = weight_variable([192,1])
-	b_fc2 = bias_variable([1])
-	y = tf.matmul(fc1_drop, W_fc2) + b_fc2
-	# y_conv=tf.maximum(tf.nn.softmax(tf.matmul(fc1_drop, W_fc2) + b_fc2),1e-30)
+	W_fc2 = weight_variable([108,2])
+	b_fc2 = bias_variable([2])
+	# y = tf.matmul(fc1_drop, W_fc2) + b_fc2
+	y_conv=tf.maximum(tf.nn.softmax(tf.matmul(fc1_drop, W_fc2) + b_fc2),1e-30)
 
 	#补丁，防止y等于0，造成log(y)计算出-inf
 	#y1 = tf.maximum(y_conv,1e-30)
 
 	#代价函数
-	# cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-	cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
+	cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+	# cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
 	# cross_entropy = tf.reduce_mean(y_*tf.log(y_conv))
 	#使用Adam优化算法来调整参数
 	train_step = tf.train.AdamOptimizer(0.5).minimize(cross_entropy)
 	
 	#测试正确率
+	correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 	# correct_prediction = tf.equal(y_conv, y_)
 	# accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-	correct_prediction = tf.equal(y, y_)
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	# correct_prediction = tf.equal(y, y_)
+	# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 	#保存模型训练数据
 	saver = tf.train.Saver()
@@ -110,7 +112,7 @@ def main():
 		#print i,'2'
 
 		#每迭代100个 batch，对当前训练数据进行测试，输出当前预测准确率
-		if i%1000 == 0:
+		if i%100 == 0:
 			#print "test accuracy %g"%accuracy.eval(feed_dict={x: test_images, y_: test_labels, keep_prob: 1.0})
 			train_accuracy = accuracy.eval(feed_dict={x:batch_xs, y_: batch_ys, keep_prob: 1.0})
 			print "step %d, training accuracy %g"%(i, train_accuracy)
